dacf93350ac4c46117e37f265b46528fcbcf4464
StasonS <stan.stryzh@gmail.com>
2017-10-28 23:52:59 +0300

    1.sql done

--- src/main/resources/1.sql
+++ src/main/resources/1.sql
@@ -1,1 +1,8 @@
-ALTER TABLE developers ADD salary DECIMAL
+ALTER TABLE developers ADD salary DECIMAL;
+
+UPDATE developers SET salary = 2200 WHERE developer_id = 1;
+UPDATE developers SET salary = 2300 WHERE developer_id = 2;
+UPDATE developers SET salary = 2400 WHERE developer_id = 3;
+UPDATE developers SET salary = 2500 WHERE developer_id = 4;
+UPDATE developers SET salary = 2600 WHERE developer_id = 5;
+UPDATE developers SET salary = 2700 WHERE developer_id = 6;
