87958a37055e345ad4a3a694e8889e0880135844
StasonS <stan.stryzh@gmail.com>
2017-11-07 21:31:01 +0200

    Corrections in 2 and 3

--- src/main/resources/2.sql
+++ src/main/resources/2.sql
@@ -3,8 +3,6 @@ FROM
   developers d
   JOIN project_developer pd ON d.id = pd.dev_id
   JOIN projects p ON pd.project_id = p.id
-WHERE pd.project_id = p.id
-      AND pd.dev_id = d.id
 GROUP BY project_name
 ORDER BY sum(salary) DESC
 LIMIT 1
--- src/main/resources/3.sql
+++ src/main/resources/3.sql
@@ -2,5 +2,4 @@ SELECT sum(salary)
 FROM developers d
   JOIN dev_skill ds ON d.id = ds.dev_id
   JOIN skills s ON ds.skill_id = s.id
-WHERE skill_name = 'Java' AND s.id = ds.skill_id
-      AND ds.dev_id = d.id
+WHERE skill_name = 'Java'
